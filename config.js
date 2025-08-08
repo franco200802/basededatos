d5b36a28cbb2093ea03bbd37955a26a7c158e704
franco200802 <ingfranciscomarotta@gmail.com>
2025-08-08 12:05:02 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,30 @@
+/*ejercicio 1*/ 
+function tocar(){
+    if (document.getElementById("circulo").style.backgroundColor == "red") {
+    document.getElementById("circulo").style = "background-color:blue";
+    console.log("ahora es azul");
+    }
+    else{
+    document.getElementById("circulo").style = "background-color:red";
+    console.log("ahora es rojo");
+    }
+}
+
+/*ejercicio 2*/ 
+function hexa() {
+    let valor = document.getElementById("dato_entrante").value;
+
+    if (valor[0] != "#") {
+        valor = "#" + valor;
+    }
+    if (valor.length == 7) {
+        document.getElementById("circulo").style.backgroundColor = valor;
+    }
+    else{
+        alert("no es un codigo valido");
+    }
+
+
+}
+
+
---
+++ script.js
@@ -0,0 +1,4 @@
+function submit() {
+    //let valor = document.getElementById("input1").value;
+    console.log("submit");
+}
