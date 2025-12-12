7edc76c246ada36c23d4bbe969b3f005196433d2
franco200802 <ingfranciscomarotta@gmail.com>
2025-12-11 23:30:06 -0300

    Add files via upload

---
+++ ejrecu1.sql
@@ -0,0 +1,63 @@
+/*1. Se desea implementar un procedimiento que permita a los clientes realizar una reserva
+con descuento por fidelidad. La promoción es válida sólo si el cliente tiene más de 1000
+puntos y la habitación tiene un precio base mayor a $50.000. Si se cumplen ambas
+condiciones, se realiza la reserva aplicando un 15% de descuento, se insertan los datos
+correspondientes y se envía una notificación con el mensaje "Reserva realizada con
+descuento".*//
+
+DELIMITER //
+CREATE PROCEDURE reserva_fidelidad (IN idCliente INT, IN idHabitacion INT, IN fechaInicio
+DATE, IN fechaFin DATE)
+BEGIN
+START TRANSACTION;
+select puntos into p from clientes where id = idCliente;
+select precio_base into preciob from tipos_habitacion join habitacion on idTipo=id where idHabitacion = habitaciones.idHabitacion;
+select precio_final into precio_f from reservas join clientes on idcliente = cliente.idcliente where idCliente= clientes.idCliente;
+insert into reservas values (null, idCliente, idHabitacion, null, null, null,precio_f * 0,75 );
+if p > 1000 and preciob > 50000 then
+commit;
+else
+rollback;
+end if;
+END//
+
+
+/*Triggers
+1. Crear un trigger que, al crearse una reserva, verifique que no se superponga con otra
+reserva activa en la misma habitación. Si existe otra reserva con fechas que se
+superponen, debe lanzar el error:"Error: la habitación ya está reservada en ese
+período.¨*/
+
+
+create trigger before_create_reserva before insert on reservas for each row
+begin 
+if exists (select * from reservas where idHabitacion= new.idHabitacion and fecha_fin > new.fecha_inicio) then 
+signal sqlstate "45000" set message_text = "Error: la habitación ya está reservada en ese
+período.";
+end if;
+end//
+
+
+/*2. Crear un trigger que, al marcarse una reserva como finalizada, actualice el estado de la
+habitación a Disponible. Validar correctamente que la reserva esté finalizada.*/
+
+create trigger after_update_reserva after update on reservas for each row 
+begin
+if datediff(fecha_fin, current_date() ) >=0 and new.estado = "finalizado" then 
+update habitaciones set estado = "Disponible" where new.idHabitacion = habitaciones.id;
+end if;
+end//
+
+
+
+/*3. Crear un trigger que, al actualizarse el precio base de una habitación, incremente un
+10% el precio de las reservas futuras (con fecha_inicio posterior a la fecha actual) para
+esa habitación.
+*/
+
+create trigger after_update_habitacion after update on habitacion for each row
+begin
+if old.precio<new.precio then 
+update reservas set precio_final =precio_final * 1.1 where reservas.idHabitacion = new.idHabitacion and datediff(fecha_inicio,current_date())>=0;
+end if;
+end//
---
+++ ejrecu2.sql
@@ -0,0 +1,58 @@
+/*Triggers
+1. Crear un trigger que al realizarse un préstamo, verifique que el ejemplar no esté ya
+prestado y que el socio no tenga otro préstamo sin devolver. Si sucede alguna de las dos
+debe lanzar el error:"Error: el ejemplar ya se encuentra prestado o tiene otro préstamo
+activo".*/
+
+create trigger before_insert_prestamo before insert on prestamo for each row
+begin
+if exists (select * from ejemplar where estado = "Disponible" and idEjemplar = new.idEjemplar ) and 
+exists (select * from socio join prestamo on idSocio=socio.idSocio
+where datediff(fechafin, current_date()) >= 0 and idSocio= new.idSocio)
+signal sqlstate "45000" set message_text ="Error: el ejemplar ya se encuentra prestado o tiene otro préstamo
+activo";
+end if;
+end//
+
+
+/*2. Crear un trigger que al devolverse un libro cambie su estado a disponible. Además, si la
+devolución ocurre con más de 2 días de retraso se le debe agregar una multa de $1000
+pesos.*/
+
+create trigger after_libro after update on libro for each row
+begin
+update ejemplar join libro on idLibro= new.libro.idLibro set estado = "disponible" where new.idLibro= idLibro;
+
+
+
+
+/*3. Crear un trigger que al aumentar el ranking de un libro reduzca la duración del
+préstamo en un 10%, si el nuevo ranking es mayor a 8. Validar que el ranking haya
+cambiado.*/
+
+
+
+
+
+/*Transacciones
+1. Se desea implementar un procedimiento que permita al socio obtener un préstamo
+gratuito si tiene más de 1000 puntos y si el libro tiene un ranking mayor a 5. Si se
+cumplen las condiciones se debe crear el préstamo, marcar el libro como prestado y
+enviarle al socio la notificación:"¡Felicitaciones! Obtuviste un préstamo gratuito"*/
+
+
+DELIMITER //
+CREATE PROCEDURE prestamo_gratis (IN idSocio INT, IN idLibro INT)
+BEGIN
+START TRANSACTION;
+set mesagge = "¡Felicitaciones! Obtuviste un préstamo gratuito";
+select puntos into p from socio where idSocio = socio.idSocio;
+select ranking into r from libro join ejemplar on idLibro = ejemplar.idLibro where idLibro = ejemplar.idLibro;
+
+insert into prestamo values (null, idsocio, idLibro, current_date(), null);
+if p > 1000 and r > 5 then 
+commit;
+else 
+rollback;
+end if;
+END//
